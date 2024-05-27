b15d54a79436d2695e2868abe3e9f88328518a07
moreira.geo <moreira.geo@gmail.com>
2024-05-27 18:00:06 -0300

    fix bootstrap

--- functions/bootstrap/utils/010_schema.sql
+++ functions/bootstrap/utils/010_schema.sql
@@ -9,15 +9,16 @@ ALTER DEFAULT PRIVILEGES IN SCHEMA ofpnd GRANT SELECT ON SEQUENCES TO ${DB_SOLUT
 
 
 CREATE TABLE ofpnd.floresta_publica_nao_destinada (
-	codigo TEXT NOT NULL,
-	nome text NOT null default 'Desconhecido',
+	codigo text NOT NULL,
+	nome text DEFAULT 'Desconhecido'::text NOT NULL,
 	esfera text NOT NULL,
 	orgao text NOT NULL,
 	estagio text NOT NULL,
 	estado text NOT NULL,
-	ano integer NOT NULL,
+	ano int4 NOT NULL,
 	bioma text NOT NULL,
-	geom geometry(multipolygon, 4674) NOT NULL,
+	geom public.geometry(multipolygon, 4674) NOT NULL,
+	uf text NULL,
 	CONSTRAINT floresta_publica_nao_destinada_pkey PRIMARY KEY (codigo)
 );
 
@@ -25,7 +26,7 @@ CREATE TABLE ofpnd.desmatamento (
 	codigo text NOT NULL,
 	data date NOT null,
 	fonte text NOT NULL,
-    	area_ha float NOT NULL,
+    area_ha float NOT NULL,
 	CONSTRAINT desmatamento_pkey PRIMARY KEY (codigo, data, fonte),
 	CONSTRAINT floresta_publica_nao_destinada_fkey FOREIGN KEY (codigo) REFERENCES ofpnd.floresta_publica_nao_destinada(codigo) ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -34,19 +35,19 @@ CREATE TABLE ofpnd.desmatamento (
 
 CREATE TABLE ofpnd.informacao (
 	codigo text NOT NULL,
-	fpnd_area_ha float NOT NULL,
-	carbono_estoque_ton float NOT NULL,
-	carbono_densidade_ton_ha float NOT NULL,
-	carbono_equivalencia_co2_ton_ha float NOT NULL,
-	especie_diversidade_media float NOT NULL,
-	car_sobreposicao_fpnd_ha float NOT NULL,
-	car_sobreposicao_fpnd_per float NOT NULL,
-	mineracao_area_ha float NOT NULL,
-	floresta_original_area_ha float NOT NULL,
-	floresta_atual_area_ha float NOT NULL,
-	floresta_atual_area_per float NOT NULL,	
-	desmatamento_area_ha float NOT NULL,
-	desmatamento_area_per float NOT null,
+	fpnd_area_ha float8 NOT NULL,
+	carbono_estoque_ton float8 NOT NULL,
+	carbono_densidade_ton_ha float8 NOT NULL,
+	carbono_equivalencia_co2_ton_ha float8 NOT NULL,
+	especie_diversidade_media float8 NOT NULL,
+	car_sobreposicao_fpnd_ha float8 NOT NULL,
+	car_sobreposicao_fpnd_per float8 NOT NULL,
+	mineracao_area_ha float8 NOT NULL,
+	floresta_original_area_ha float8 NOT NULL,
+	floresta_atual_area_ha float8 NOT NULL,
+	floresta_atual_area_per float8 NOT NULL,
+	desmatamento_area_ha float8 NOT NULL,
+	desmatamento_area_per float8 NOT NULL,
 	CONSTRAINT informacao_pkey PRIMARY KEY (codigo),
 	CONSTRAINT floresta_publica_nao_destinada_fkey FOREIGN KEY (codigo) REFERENCES ofpnd.floresta_publica_nao_destinada(codigo) ON DELETE CASCADE ON UPDATE CASCADE
 );
